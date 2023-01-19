8ed8ab73fc7763e00b497e91df6134cde3c3a836
seokhee123 <82377493+seokhee123@users.noreply.github.com>
2023-01-20 01:16:25 +0900

    주변환경
    milk juice 추가
    food 추가
    창밖 배경 변경 추가
    rabbit animation curve

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -24,6 +24,30 @@ public class GameManager : MonoBehaviour
     public GameObject watermelonPos2;
     bool isWatermelon = false;
 
+    public GameObject milk;
+    public GameObject milkPos;
+    bool isMilk = false;
+    public GameObject juice;
+    public GameObject juicePos;
+    bool isJuice = false;
+
+    public GameObject[] food;
+
+    public GameObject dirtyGround;
+    public GameObject clearSky;
+
+    public GameObject bowlWater;
+
+    public GameObject rabbit;
+    bool isRabbit = false;
+    public GameObject rabbitPos1;
+    public GameObject rabbitPos2;
+    public GameObject rabbitPos3;
+
+    [SerializeField]
+    public AnimationCurve curve1;
+    public AnimationCurve curve2;
+
     void Start()
     {
         StartCoroutine("GameStart");
@@ -67,6 +91,87 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
         isMelon = true;
         isWatermelon = true;
+        yield return new WaitForSeconds(1f);
+        isMilk = true;
+        isJuice = true;
+        yield return new WaitForSeconds(1f);
+        StartCoroutine("Food");
+    }
+
+    IEnumerator Food()
+    {
+        for(int i = 0; i < food.Length; i++)
+        {
+            for (float f = 0f; f < 1; f += 0.02f)
+            {
+                Color c = food[i].GetComponent<SpriteRenderer>().color;
+                c.a = f;
+                food[i].GetComponent<SpriteRenderer>().color = c;
+                yield return null;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+        yield return new WaitForSeconds(1f);
+        StartCoroutine("Window");
+    }
+
+    IEnumerator Window()
+    {
+        for (float f = 0f; f < 1; f += 0.01f)
+        {
+            Color c = clearSky.GetComponent<SpriteRenderer>().color;
+            c.a = f;
+            clearSky.GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        StartCoroutine("Window2");
+    }
+
+    IEnumerator Window2() { 
+        for (float t = 1f; t > 0; t -= 0.01f)
+        {
+            Color cc = dirtyGround.GetComponent<SpriteRenderer>().color;
+            cc.a = t;
+            dirtyGround.GetComponent<SpriteRenderer>().color = cc;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        StartCoroutine("Bowl");
+    }
+
+    IEnumerator Bowl()
+    {
+        for (float t = 1f; t > 0; t -= 0.01f)
+        {
+            Color cc = bowlWater.GetComponent<SpriteRenderer>().color;
+            cc.a = t;
+            bowlWater.GetComponent<SpriteRenderer>().color = cc;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        StartCoroutine("Rabbit");
+    }
+
+    IEnumerator Rabbit()
+    {
+        float duration = 1f;
+        float time = 0.0f;
+        Vector2 start = rabbit.transform.position;
+        Vector2 end = rabbitPos1.transform.position;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float linearT = time / duration;
+            float heightT = curve1.Evaluate(linearT);
+
+            float height = Mathf.Lerp(0.0f, 2, heightT);
+
+            rabbit.transform.position = Vector2.Lerp(start, end, linearT) + new Vector2(0.0f, height);
+
+            yield return null;
+        }
     }
 
     private void Update()
@@ -78,6 +183,18 @@ public class GameManager : MonoBehaviour
         if(isWatermelon == true)
         {
             watermelon.transform.position = Vector2.Lerp(watermelon.transform.position, watermelonPos2.transform.position, 0.01f);
+        }
+        if(isMilk == true)
+        {
+            milk.transform.position = Vector2.Lerp(milk.transform.position, milkPos.transform.position, 0.01f);
+        }
+        if (isJuice == true)
+        {
+            juice.transform.position = Vector2.Lerp(juice.transform.position, juicePos.transform.position, 0.01f);
+        }
+        if(isRabbit == true)
+        {
+            //rabbit.transform.position = Vector2.Lerp(rabbit.transform.position, rabbitPos1.transform.position, 0.01f);
         }
     }
 }
